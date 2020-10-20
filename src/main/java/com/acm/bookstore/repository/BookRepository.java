65095292e483077239ce8c5530d66d40d4fc170b
Augusto Cesar <cesargt92@gmail.com>
2020-10-20 15:53:31 -0300

    Criação de entidades book e autor

--- src/main/java/com/acm/bookstore/repository/BookRepository.java
+++ src/main/java/com/acm/bookstore/repository/BookRepository.java
@@ -2,6 +2,8 @@ package com.acm.bookstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.acm.bookstore.entity.Book;
+
 public interface BookRepository extends JpaRepository<Book, Long>{
 
 }
